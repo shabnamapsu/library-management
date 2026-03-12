ca30ab9bdb8ceb23584c4d4e21d2c9891d0b091f
shabnamapsu <ahanak748@gmail.com>
2026-03-12 19:59:07 +0530

    fix url server

--- server/index.js
+++ server/index.js
@@ -61,7 +61,7 @@ app.use(express.static(path.join(__dirname, "../client/dist")));
 
 
 // React Router Fix (VERY IMPORTANT)
-app.get("/./", (req, res) => {
+app.get("/.*/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
